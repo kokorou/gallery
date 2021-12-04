bf224d750abbdb82d7c634aac5e7f9c3abc0d2d8
kokorou <liwo8owil@gmail.com>
2021-12-05 06:05:27 +0900

    new function

--- src/components/MainNavBar.js
+++ src/components/MainNavBar.js
@@ -1,11 +1,32 @@
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import '../CSS/MainNavbar.css'
+import {MdMenu } from 'react-icons/md';
 
 const MainNavbar =() =>{
+
+    const [toggleMenu, setToggleMenu] = useState(false)
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu)
+    }
+
+    useEffect(() => {
+        const changeWidth = () => {
+            setScreenWidth(window.innerWidth);
+        }
+        window.addEventListener('resize', changeWidth)
+
+        return () => {
+            window.removeEventListener('resize', changeWidth)
+        }
+    }, [])
+
     return (
         <header>
             <div class="logo">Gallery</div>
             <nav>
+                {(toggleMenu || screenWidth > 800) && (
                 <ul>
                     <li>
                         <Link to='/'>Main</Link>
@@ -23,6 +44,8 @@ const MainNavbar =() =>{
                         <Link to='/about'>About</Link>
                     </li>
                 </ul>
+                )}
+                <button onClick={toggleNav} className="btn"><MdMenu/></button>
             </nav>
         </header>
     )
